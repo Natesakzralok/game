ac42ee04610740b0239c5f1c0f1fcbca16d62e1d
Natesakzralok <natalie.sedlackova@icloud.com>
2025-06-04 09:46:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let player = {
 let gravity = 0.5;
 let obstacles = [];
 let frame = 0;
+let score = 0;
 
 function drawHeart(x, y) {
   ctx.fillStyle = "#ff4da6";
@@ -45,36 +46,55 @@ function createObstacle() {
   let height = 30;
   let x = canvas.width;
   let y = canvas.height - height;
-  obstacles.push({ x, y, width, height });
+  obstacles.push({ x, y, width, height, passed: false });
 }
 
 function updateObstacles() {
   for (let i = 0; i < obstacles.length; i++) {
-    obstacles[i].x -= 5;
+    let obs = obstacles[i];
+    obs.x -= 5;
 
+    // Detekce kolize
     if (
-      player.x < obstacles[i].x + obstacles[i].width &&
-      player.x + player.width > obstacles[i].x &&
-      player.y < obstacles[i].y + obstacles[i].height &&
-      player.y + player.height > obstacles[i].y
+      player.x < obs.x + obs.width &&
+      player.x + player.width > obs.x &&
+      player.y < obs.y + obs.height &&
+      player.y + player.height > obs.y
     ) {
       alert("Prohrála jsi! Obnov stránku pro novou hru.");
       document.location.reload();
+    }
+
+    // Započítání skóre
+    if (!obs.passed && obs.x + obs.width < player.x) {
+      obs.passed = true;
+      score++;
     }
   }
 
   obstacles = obstacles.filter(obs => obs.x + obs.width > 0);
 }
 
+function drawScore() {
+  ctx.fillStyle = "#ff1493";
+  ctx.font = "24px Comic Sans MS";
+  ctx.fillText("Skóre: " + score, 20, 40);
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Hráč
   ctx.fillStyle = player.color;
   ctx.fillRect(player.x, player.y, player.width, player.height);
 
+  // Překážky
   for (let i = 0; i < obstacles.length; i++) {
     drawHeart(obstacles[i].x + 5, obstacles[i].y);
   }
+
+  // Skóre
+  drawScore();
 }
 
 function gameLoop() {
