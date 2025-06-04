4176b6ddffbc20522938356448fcf8a0f13cf8c2
Natesakzralok <natalie.sedlackova@icloud.com>
2025-06-04 09:03:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const character = document.getElementById("character");
-const block = document.getElementById("block");
+const blocks = document.querySelectorAll(".block");
 
 function jump() {
-  if (character.classList != "jump") {
+  if (!character.classList.contains("jump")) {
     character.classList.add("jump");
-    setTimeout(function () {
+    setTimeout(() => {
       character.classList.remove("jump");
     }, 500);
   }
 }
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", () => {
   jump();
 });
 
-let checkDead = setInterval(function () {
-  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-  let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+setInterval(() => {
+  blocks.forEach((block) => {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let blockWidth = parseInt(window.getComputedStyle(block).getPropertyValue("width"));
 
-  if (blockLeft < 100 && blockLeft > 0 && characterTop >= 130) {
-    alert("Konec hry! Zkus to znovu.");
-  }
+    if (blockLeft < 80 && blockLeft > 20 && characterTop >= 130) {
+      alert("Konec hry! Zkus to znovu.");
+    }
+  });
 }, 10);
